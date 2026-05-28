32cedfe9436a7ddb08e79a1797995d6f43ca00b9
Putrabungsu6 <muhammadnorseputra@gmail.com>
2026-05-28 17:01:49 +0800

    fix: proxy.ts

--- src/services/session-store.ts
+++ src/services/session-store.ts
@@ -1,21 +1,10 @@
 "use server";
 
 import UserInfo from "@/data/user-info";
-import { AES, enc } from "crypto-js";
-import { cookies } from "next/headers";
 
 export async function getSessionFromDatabase(token: string) {
-  const cookieLocal = await cookies();
-
-  const token_plain = AES.decrypt(
-    cookieLocal.get("sso_token")?.value as string,
-    process.env.KEY_PASSPHRASE as string,
-  ).toString(enc.Utf8);
-
-  const session = token || token_plain;
-
   const sessionDB = await UserInfo({
-    access_token: session,
+    access_token: token,
   });
 
   if (!sessionDB.response.status) {
